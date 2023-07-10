8f6e1511a549ab43e57d91d8a0c034200fdbcdf7
David Macdougall <djmacdougall@gmail.com>
2023-07-10 14:24:16 +0200

    Implimented employee data update functionality

--- src/App.js
+++ src/App.js
@@ -5,40 +5,59 @@ import { v4 as uuidv4 } from "uuid";
 
 
 function App() {
-  const [role, setRole] = useState();
   const [employee, setEmployee] = useState([
     {
+      id: "1",
       name: "Alice Bailey",
       role: "Developer",
       img: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg",
     },
     {
+      id: "2",
       name: "Bob Smith",
       role: "Lead Developer",
       img: "https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg",
     },
     {
+      id: "3",
       name: "Devindra Naidoo",
       role: "Designer",
       img: "https://images.pexels.com/photos/2586823/pexels-photo-2586823.jpeg",
     },
     {
+      id: "4",
       name: "Mia Murray",
       role: "Marketing",
       img: "https://images.pexels.com/photos/2587112/pexels-photo-2587112.jpeg",
     },
     {
+      id: "5",
       name: "Maggie Smith",
       role: "Project Manager",
       img: "https://images.pexels.com/photos/6001808/pexels-photo-6001808.jpeg",
     },
   ]);
+
+  function updateEmployee (id, newName, newRole) {
+    setEmployee(
+      employee.map((emp) => {
+        if (emp.id === id) {
+          return {
+            ...emp,
+            name: newName,
+            role: newRole,
+          };
+        }
+        return emp;
+      })
+    );
+  
+  }
   return (
     <div className="App">
-      <input type="text" onChange={(e) => setRole(e.target.value)} />
       <div className="flex flex-wrap justify-center">
         {employee.map((emp) => (
-          <Employee key={uuidv4()} {...emp} />
+          <Employee key={uuidv4()} {...emp} updateEmployee={updateEmployee} />
         ))}
       </div>
     </div>
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function EditEmployee() {
+function EditEmployee(props) {
   const [show, setShow] = useState(false);
+
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -27,7 +29,14 @@ function EditEmployee() {
           <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <form id="edit-employee-form" className="bg-white px-8 pt-6 pb-8">
+            <form 
+              onSubmit={(e) => {
+                e.preventDefault();
+                props.updateEmployee(props.id, name, role)
+                handleClose();
+              }}
+              id="edit-employee-form" 
+              className="bg-white px-8 pt-6 pb-8">
               <div className="mb-4">
                 <label
                   className="block text-gray-700 text-sm font-bold mb-2"
@@ -38,7 +47,8 @@ function EditEmployee() {
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="Name"
                   type="text"
-                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
               <div className="mb-4">
@@ -51,7 +61,8 @@ function EditEmployee() {
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="Role"
                   type="text"
-                  placeholder="Role"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
                 />
               </div>
             </form>
