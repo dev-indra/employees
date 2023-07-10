8fcdc9b09c04c06cda4475156776430cd146f8c0
David Macdougall <djmacdougall@gmail.com>
2023-07-10 13:32:07 +0200

    Added form to change employee values

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import './App.css';
-import Employee from './components/Employee';
-import { useState } from 'react';
+import "./App.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 
 function App() {
   const [role, setRole] = useState();
+  const [employee, setEmployee] = useState([
+    {
+      name: "Alice Bailey",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg",
+    },
+    {
+      name: "Bob Smith",
+      role: "Lead Developer",
+      img: "https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg",
+    },
+    {
+      name: "Devindra Naidoo",
+      role: "Designer",
+      img: "https://images.pexels.com/photos/2586823/pexels-photo-2586823.jpeg",
+    },
+    {
+      name: "Mia Murray",
+      role: "Marketing",
+      img: "https://images.pexels.com/photos/2587112/pexels-photo-2587112.jpeg",
+    },
+    {
+      name: "Maggie Smith",
+      role: "Project Manager",
+      img: "https://images.pexels.com/photos/6001808/pexels-photo-6001808.jpeg",
+    },
+  ]);
   return (
     <div className="App">
-      <input type="text" onChange={e => setRole(e.target.value)}/>
-      <Employee name="Sachin" role="Developer" age="25" />
-      <Employee name="Devindra" role={role} age="22" />
-      <Employee name="David" role="" age="50" />
+      <input type="text" onChange={(e) => setRole(e.target.value)} />
+      <div className="flex flex-wrap justify-center">
+        {employee.map((emp) => (
+          <Employee key={uuidv4()} {...emp} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/EditEmployee.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+function EditEmployee() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <button
+        onClick={handleShow}
+        className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+      >
+        Update
+      </button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update Employee</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <form id="edit-employee-form" className="bg-white px-8 pt-6 pb-8">
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Name
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="Name"
+                  type="text"
+                  placeholder="Name"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Role
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="Role"
+                  type="text"
+                  placeholder="Role"
+                />
+              </div>
+            </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleClose}>Close</button>
+          <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" form="edit-employee-form">Update</button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default EditEmployee;
